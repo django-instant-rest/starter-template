279736b41b8f09f7865168a432b8a8c6cdd257b4
unknown <jbj1996@gmail.com>
2020-12-07 11:43:57 -0500

    introduced time stamped models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django_instant_rest.models import TimeStampedModel
 
-# Create your models here.
+class Author(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length= 255, unique=True)
+
+class Book(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length= 255, unique=True)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
