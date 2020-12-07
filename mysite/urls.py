6bc3e6ac62efb873188ed06e656d1f67fed2a00d
unknown <jbj1996@gmail.com>
2020-12-07 12:27:19 -0500

    established pipfile and piplock file

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from myapp.models import *
+from django_instant_rest import patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    patterns.resource('authors', Author),
+    patterns.resource('books', Book),
 ]
