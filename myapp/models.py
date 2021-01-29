d30c09a9bbc91336a479c7f24fc52f0899c5a713
unknown <jbj1996@gmail.com>
2021-01-28 23:10:46 -0500

    new migrants

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django_instant_rest.models import TimeStampedModel
+from django_instant_rest.models import RestResource
 
-class Author(TimeStampedModel):
+class Author(RestResource):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length= 255, unique=True)
 
-class Book(TimeStampedModel):
+class Book(RestResource):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length= 255, unique=True)
     author = models.ForeignKey(
