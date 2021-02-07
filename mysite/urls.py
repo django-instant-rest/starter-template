9352a28a2663d4715d30964760c3d1bcd959897a
craig fay <craigfay1@gmail.com>
2021-02-07 16:31:58 -0600

    Externalize new features from django-instant-rest

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from django_instant_rest import patterns
 urlpatterns = [
     patterns.resource('authors', Author),
     patterns.resource('books', Book),
+    patterns.resource('users', User),
+    patterns.client('users', User),
 ]
