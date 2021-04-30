d4428ad8e50832370beb3a56acf964c7962f3be7
craig fay <craigfay1@gmail.com>
2021-04-30 17:23:40 -0500

    Introduce liberal cross origin policy middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.LiberalCrossOriginPolicy',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
